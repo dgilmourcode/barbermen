2c9c71488a97be3990d146c50b137185db2ae8ea
dgilmourcode <dgilmourcode@gmail.com>
2026-07-28 16:48:31 -0300

    feat: add Barba com Máquina to gallery (gallery-13.jpg)

--- src/components/ProfessionalGallery.tsx
+++ src/components/ProfessionalGallery.tsx
@@ -16,6 +16,7 @@ const galleryImages = [
   { src: '/images/gallery-10.jpg', title: 'Corte Infantil', category: 'Cabelo' },
   { src: '/images/gallery-11.jpg', title: 'Hidratação Premium', category: 'Tratamento' },
   { src: '/images/gallery-12.jpg', title: 'Hidratação Clássica', category: 'Tratamento' },
+  { src: '/images/gallery-13.jpg', title: 'Barba com Máquina', category: 'Barba' },
 ];
 
 export default function ProfessionalGallery() {
